e5c97279c995df699f88d95e2170fa63c1c5423f
moonslider <moonslider@users.noreply.github.com>
2015-05-07 18:19:18 +0800

    simple mvn project

---
+++ src/main/java/xxx/moonslider/App.java
@@ -0,0 +1,13 @@
+package xxx.moonslider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
